2435ec06dc6688a1ce6f565032432a6a351e47f3
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-12 23:14:23 +0530

    added state for fields

--- src/Register.jsx
+++ src/Register.jsx
@@ -9,6 +9,25 @@ const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
 
 
 const Register = () => {
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [validName, setValidName] = useState(false)
+    const [userFocus, setuserFocus] = useState(false)
+
+    const [pwd, setPwd] = useState('')
+    const [validPws, setValidPwd] = useState(false)
+    const [pwdFocus, setPwdFocus] = useState(false)
+
+    const [matchPwd, setMatchPwd] = useState('')
+    const [validMatch, setValidMatch] = useState(false)
+    const [matchFocus, setMatchFocus] = useState(false)
+
+    const [errMsg, setErrMsg] = useState('')
+    const [success, setSuccess] = useState(false);
+
+
     return (
         <section>
 
