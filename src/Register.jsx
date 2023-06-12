e41a10f299d4ad779b1c14adff2ae28538ab9627
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-12 22:24:31 +0530

    imported required dependencies

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { useRef, useState, useEffect } from 'react'
+import { faCheck, faTimes, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Register = () => {
     return (
-        <div>Register</div>
+        <section>
+
+        </section>
     )
 }
 
