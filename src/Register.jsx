8e18c030391f662e31be2a8c2401b44b02462e36
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-12 22:31:18 +0530

    added regex for username and password validation

--- src/Register.jsx
+++ src/Register.jsx
@@ -2,6 +2,12 @@ import { useRef, useState, useEffect } from 'react'
 import { faCheck, faTimes, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+// RegEx for validation
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
+
+
+
 const Register = () => {
     return (
         <section>
